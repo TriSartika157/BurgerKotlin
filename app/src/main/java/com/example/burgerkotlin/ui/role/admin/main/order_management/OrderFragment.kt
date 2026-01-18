ce0ddc7b9ddd9af89e657d0a459fa6c35a9d1065
Dewa Tri Wijaya <dewatwc@gmail.com>
2026-01-18 14:09:33 +0700

    add

--- app/src/main/java/com/example/burgerkotlin/ui/role/admin/main/order_management/OrderFragment.kt
+++ app/src/main/java/com/example/burgerkotlin/ui/role/admin/main/order_management/OrderFragment.kt
@@ -9,6 +9,7 @@ import com.example.burgerkotlin.R
 
 class OrderFragment : Fragment() {
 
+    //create
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
